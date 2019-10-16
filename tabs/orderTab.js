dab70e871acd6c29b3a933e204e46c19b17e7759
Amr-Reda <amrreda547@gmail.com>
2019-10-17 01:39:06 +0200

    add navigate button in order

--- tabs/orderTab.js
+++ tabs/orderTab.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Text, View } from 'react-native'
-import { Header } from 'react-native-elements'
+import { Header, Button } from 'react-native-elements'
 
 class OrderScreen extends React.Component {
 	render() {
@@ -10,6 +10,12 @@ class OrderScreen extends React.Component {
 				<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
 					<Text style={{ fontSize: 20 }}>No checks reserved Now!</Text>
 					<Text style={{ fontSize: 15 }}>check our Labs!</Text>
+				</View>
+				<View style={{ marginBottom: 100  }}>
+					<Button
+						title='Reserve Check Package'
+						onPress={() => this.props.navigation.navigate('Tests')}
+					/>
 				</View>
 			</View>
 		)
